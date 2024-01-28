0de858d754c4fff4c1a099aa2b16e469b3979065
Natnael <natnaeltilahun157@gmail.com>
2024-01-28 17:21:15 +0300

    setup widget to be compiled into 1 js

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2015",
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
