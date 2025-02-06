ad23cb6fdff647d995944b3c86742027b8280a94
Natnael <natnaeltilahun157@gmail.com>
2025-02-06 14:55:25 +0100

    ui upgrade

---
+++ frontend/app/dashboard/chats/[slug]/widget/triangle.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Triangle = () => {
+  return (
+    <svg
+      width="32"
+      height="10"
+      viewBox="0 0 32 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.5 9.99992C0.5 9.99992 11.3721 5.55816 11.3721 1.11657C11.3721 -2.35259 31.1977 9.99992 31.1977 9.99992H0.5Z"
+        fill="#D3D3D3"
+      />
+    </svg>
+  );
+};
+
+export default Triangle;
