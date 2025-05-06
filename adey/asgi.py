c5feed1cc3f9f634e81649d220c4abdd5cb5ee9d
Natnael <natnaeltilahun157@gmail.com>
2025-05-06 02:15:02 +0200

    update asgi setting

--- adey/asgi.py
+++ adey/asgi.py
@@ -9,15 +9,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from adey import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adey.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from adey import routing
+
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
